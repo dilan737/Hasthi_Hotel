e659ff582686ff8afc4046c928269f841f0c0d39
Dilan <dilanavishka070@gmail.com>
2025-08-10 00:36:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import mongoose from "mongoose";
 import studentRouter from "./routes/studentRouter.js";
 //import student from "./models/Student.js";
 
+//mongodb+srv://admin:<db_password>@cluster0.sfdum3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 const app=express();
 
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.lotgyds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+mongoose.connect("mongodb+srv://admin:1234@cluster0.sfdum3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
     ()=>{
 
         console.log("Connected To the database");
