e8ec87ecc3b9cc3df658cd9fe2d4f6f49c030677
Dilan <dilanavishka070@gmail.com>
2025-08-09 15:17:57 +0530

    get and put method

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
 import express from "express"; 
 import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import student from "./models/Student.js";
 
-let app=express();
+const app=express();
 
 app.use(bodyParser.json())
 
+mongoose.connect("mongodb+srv://admin:123@cluster0.lotgyds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+    ()=>{
+
+        console.log("Connected To the database");
+    }
+).catch(
+    ()=>{
+        console.log("Connection Failed");
+    }
+)
+
 app.get("/",
+    (req,res)=>{
+
+        student.find().then(
+            (students)=>{
+                res.json(students)
+            }
+        ).catch(
+            ()=>{
+                message:"An error Occured"
+            }
+
+        )
+
+
+      
+    })
+app.post("/",
+    (req,res)=>{
+
+      
+
+        const student=new student(req.body)
+
+        student.save().then(
+            ()=>{
+
+                res.json({
+                    message:"Student Saved succesfully"
+                })
+            }
+        ).catch(
+            ()=>{
+                res.json(
+                    {
+                        message:"Student  Saved Failed"
+                    }
+                )
+            }
+        )
+    }
+
+
+        
+    
+        
+        
+    
+)
+
+app.delete("/",
     (req,res)=>{
         console.log(req.body)
         console.log("Get request recerived");
         res.json({message:"Hellow World"})
     })
-
+    app.put("/",
+    (req,res)=>{
+        console.log(req.body)
+        console.log("Get request recerived");
+        res.json({message:"Hellow World"})
+    })
 
 app.listen(5000,
     ()=>{
---
+++ models/Student.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    city: String
+});
+
+const student = mongoose.model("Students", studentSchema);
+
+export default student;
